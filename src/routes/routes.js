adaf14c71adef5ec038a3d5688dc972cb6687172
Adinan Alves De Brito Filho <adinanbrito@live.com>
2023-05-06 02:50:59 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,60 +4,71 @@ const bcrypt = require("bcrypt");
 const Joi = require("@hapi/joi");
 const jwt = require("jsonwebtoken");
 
-
 const registerSchema = Joi.object({
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(8).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(8).required()
 });
-  
+
+const loginSchema = Joi.object({
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(8).required()
+});
+
 router.post("/register", async (req, res) => {
     const { error } = registerSchema.validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+
+    if (error) return res.status(400).json({
+      msg: error.details[0].message.replace('"','').replace('"', '')
+    });
     
-    // Verifica se o usuário já está cadastrado
     const emailExists = await User.findOne({ email: req.body.email });
+    if (emailExists) return res.status(400).json({
+      msg: "Email already exists."
+    });
   
-    if (emailExists) return res.status(400).send("Email allready exists");
-  
-    // Aplica hash na senha
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(req.body.password, salt);
   
-    // Cria um novo usuário
     const user = new User({
-      name: req.body.name,
       email: req.body.email,
       password: hashPassword,
     });
   
     try {
-      const savedUser = await user.save();
-      res.send(savedUser);
-    } catch (err) {
-      res.status(400).send(err);
+      await user.save();
+      res.status(201).json({msg: "User created successfully."});
+    } catch (error) {
+      res.status(500).json({msg: error});
     }
-});
-
-
-const loginSchema = Joi.object({
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(8).required(),
 });
 
 router.post("/login", async (req, res) => {
     const { error } = loginSchema.validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).json({
+      msg: error.details[0].message.replace('"', '')
+    });
   
     const user = await User.findOne({ email: req.body.email });
     // console.log(user.password)
-    if (!user) return res.status(400).send("Email or password is wrong");
+    if (!user) return res.status(404).json({
+      msg: "User not found."
+    });
     
     const validPass = await bcrypt.compare(req.body.password, user.password);
-    if (!validPass) return res.status(400).send("Email or password is wrong");
+    if (!validPass) return res.status(422).json({
+      msg: "Password incorrect."
+    });
   
-    //Create and assign a token
-    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-    res.header("auth-token", token).send(token);
+    try {
+      const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+      res.header("auth-token", token).json({
+          msg: "You are successfully logged in.",
+          token: token
+        });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({msg: error});
+    }
   });
 
 module.exports = router;
